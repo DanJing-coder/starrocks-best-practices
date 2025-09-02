cc55d47f969741bce834f75c365ca581270783ae
Dan Jing <jingdan@starrocks.com>
2025-09-02 15:46:01 +0800

    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 const config = {
   title: 'StarRocks 最佳实践手册',
-  url: 'https://<your-username>.github.io',
+  url: 'https://DanJing-coder.github.io',
   baseUrl: '/starrocks-best-practices/',
-  organizationName: '<your-username>', // GitHub 用户名
+  organizationName: 'DanJing-coder', // GitHub 用户名
   projectName: 'starrocks-best-practices', // 仓库名
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
