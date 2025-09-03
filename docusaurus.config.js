c3a3e0813085d5515e9ab780c4062a14fb4a54e4
Dan Jing <jingdan@starrocks.com>
2025-09-03 14:55:59 +0800

    update docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'StarRocks 最佳实践手册',
   url: 'https://danjing-coder.github.io',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   organizationName: 'danjing-coder', // GitHub 用户名
   projectName: 'starrocks-best-practices', // 仓库名
   deploymentBranch: 'gh-pages',
@@ -10,6 +10,37 @@ const config = {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
   },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // 如果有 docs 目录，确保路径正确
+          routeBasePath: '/docs', // 如果你想让 docs 成为首页
+        },
+        blog: false, // 如果没有博客，设为 false
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  onBrokenLinks: 'ignore',
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/',          // 访问根路径 /
+            to: '/docs/intro',  // 自动跳转到 /docs/intro
+          },
+        ],
+      },
+    ],
+  ],
+
 };
 
 module.exports = config;
